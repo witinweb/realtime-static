b457379f0e6b01542b262c2b3a3cdfec0e9ee0d8
SCHEMR <schemryj@gmail.com>
2016-10-26 16:37:02 +0900

    불필요 엘리먼트 제거, 활성사용자만 표시

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -6,5 +6,31 @@ angular.module('GoodocRealtimeStatic')
     .controller('MasterCtrl', ['$scope', '$rootScope', '$window','$http', MasterCtrl]);
 
 function MasterCtrl($scope, $rootScope, $window, $http) {
-  
+  $scope.cards = [
+    {
+      'title':'Web',
+      'container':'active-users-container-1',
+      'defaultIds':{
+          'ids': 'ga:80446779'
+      }
+    },{
+      'title':'Android',
+      'container':'active-users-container-2',
+      'defaultIds':{
+          'ids': 'ga:77512838'
+      }
+    },{
+      'title':'iOS',
+      'container':'active-users-container-3',
+      'defaultIds':{
+          'ids': 'ga:84926759'
+      }
+    },{
+      'title':'서버건강지수',
+      'container':'',
+      'defaultIds':{
+          'ids': ''
+      }
+    }
+  ]
 };
